6f6f5f3ad72e3c87ddb902807c577208e5de31a3
AlexPrograms <olexiy2004@gmail.com>
2023-12-02 20:21:02 +0100

    Second Commit

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers;
+
+[ApiController]
+[Route("api/VillaAPI")]
+public class VillaAPIController : Controller
+{
+    [HttpGet]
+    public IEnumerable<VillaDTO> GetVillas()
+    {
+        return new List<VillaDTO>
+        {
+            new VillaDTO{Id=1, Name = "PoolView"}, 
+            new VillaDTO{Id=2, Name = "BeachView"}
+        };
+    }
+}
